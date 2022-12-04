4e41eb2051c0f9b97960669882ce9d0dd7637d0b
CobilasEugen <go.away.if@gmail.com>
2022-12-04 16:03:43 +0200

    snowflake

---
+++ DrawSnowFlake/DrawSnowFlake/App.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DrawSnowFlake
+{
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+	}
+}
